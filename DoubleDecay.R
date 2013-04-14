593263e451513fe93517ada50402bf3ec66e9a31
Vineeth Reddy <vineethreddy.agusta@gmail.com>
2013-04-14 10:17:29 +0530

    Update DoubleDecay.R

--- DoubleDecay.R
+++ DoubleDecay.R
@@ -2,8 +2,11 @@ DoubleDecay <- function(X, lmd_c, lmd_s)
 {
   dim_val <- dim(X)
   T <- dim_val[1]
+ 
   N <- dim_val[2]
+  
   m <- matrix(0,N,1)
+ 
 
 
   p_c <- exp(-lmd_c*(t(T-t(seq(1:T)))));
@@ -12,18 +15,25 @@ DoubleDecay <- function(X, lmd_c, lmd_s)
   p_c <- p_c[ , rep(seq_len(ncol(p_c)), N)]
   
   S_1 <- t((p_c * X))%*% X;
+  
   C <- cov2cor(S_1);
+
 
   p_s <- exp(-lmd_s*(t(T-t(seq(1:T)))));
   p_s <- p_s/sum(p_s);
   p_s <- p_s[ , rep(seq_len(ncol(p_s)), N)]
-  S_2 <- t((p_s %*% X)) %*% X;
-  R <- cov2cor(S_2)
+  
+  S_2 <- t((p_s * X)) %*% X;
+ 
+  R <- cov2cor(S_2) 
+  
+  s <- sqrt(diag(S_2))
+ 
   
   
-  s <- diag(R)
+  S <- diag(s) %*% C %*%diag(s);
   
-  S <- diag(s)*C*diag(s);
-  result <- c(m ,S);
+  result <- list(m ,S);
+  
   return(result);
 }
