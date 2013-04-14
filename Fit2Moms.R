555d4e34d1179c3bb0b6e4bd9300d0457c37b6aa
Vineeth Reddy <vineethreddy.agusta@gmail.com>
2013-04-14 10:14:59 +0530

    Update Fit2Moms.R

--- Fit2Moms.R
+++ Fit2Moms.R
@@ -6,15 +6,17 @@ Fit2Moms <- function(X,m,S)
 # 
 #  Most recent version of article and code available at
 #  http://www.symmys.com/node/150
-
+ 
  T <- dim(X)[1]
  N <- dim(X)[2]
  Aeq <- matrix(1,1,T) # constrain probabilities to sum to one...
  beq <- 1
  
+ 
+ 
  Aeq <- rbind( Aeq, t(X))  # ...constrain the first moments...
  beq <- rbind( beq, m)
- 
+
  SecMom <- S + (m %*% t(m))
  
  for(k in 1:N)
@@ -22,12 +24,13 @@ Fit2Moms <- function(X,m,S)
     for(l in k:N)
      {
        Aeq <- rbind( Aeq, t( X[,k] * X[,l])) 
+       
        beq <- rbind( beq, SecMom[k,l])
      }
   }
 p_0 <- matrix( 1, T, 1)
 
-p <- EntropyProg( p_0, [], [], Aeq, beq)
+p <- EntropyProg( p_0, , , Aeq, beq)
 
 return(p)
 
