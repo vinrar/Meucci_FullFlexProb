6b46686efcef3839df7d6ef23813c1d215d5d532
Vineeth Reddy <vineethreddy.agusta@gmail.com>
2013-04-15 20:43:36 +0530

    Update CallPrice.R

--- CallPrice.R
+++ CallPrice.R
@@ -1,6 +1,5 @@
-CallPrice <- function(P, K, r, t, s)
-{
-  d_1=log(P/K)+(r+s^2/2)*t;
-  d_2=d_1-s*sqrt(t);
-  return (P * pnorm(d_1) - K * exp( -r * t) * pnorm(d_2))
+CallPrice <- function(P, K, r, t, s) {
+    d_1 <- log(P / K)+( r + s^2 / 2) * t;
+    d_2 <- d_1 - s * sqrt(t);
+    return (P * pnorm(d_1) - K * exp( -r * t) * pnorm(d_2))
 }
